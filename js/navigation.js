676eddeb071f4c7293465570dc2a391a3bf4c47d
gadarohan17 <gadarohan17@gmail.com>
2016-10-20 12:22:16 +0530

    kilear

--- js/navigation.js
+++ js/navigation.js
@@ -1,5 +1,5 @@
 // var adminURL = "http://146.148.42.15:83/api/";
-var adminURL = "http://192.168.0.105:1337/api/";
+var adminURL = "http://192.168.0.107:1337/api/";
 var uploadurl = adminURL + "upload/";
 var openTab = "http://wohlig.co.in/sfanodeback/#/showstudent";
 // var openTab = "http://localhost:808/#/showstudent"
